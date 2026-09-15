ec722fad16123f31e4d064f2d8ee3e9db6c9eb49
pickyengineer1 <43855666+pickyengineer1@users.noreply.github.com>
2026-09-15 16:09:46 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,8 @@
-const CACHE='oliva-v4-1';
+const CACHE='oliva-v5-mobile';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{
-  if(e.request.mode==='navigate'){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));
-  }else{
-    e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
-  }
+ if(e.request.mode==='navigate')e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+ else e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)));
 });
